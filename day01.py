0249a6696c2e364e4b540cdfbec8b7a4e10dcc7b
parody-error <armsitad@gmail.com>
2019-12-10 17:02:03 -0700

    Adding solution for Day 1

--- day01.py
+++ day01.py
@@ -1,4 +1,28 @@
 
+from math import floor
+
+input_path = 'input.txt'
+
+
+def fuel_mass(mass):
+    return int(floor(mass)/3 - 2)
+
+
+def total_fuel_for_mass(module_mass):
+    total_mass = fuel_mass(module_mass)
+    required_fuel_mass = fuel_mass(total_mass)
+    while required_fuel_mass > 0:
+        total_mass += required_fuel_mass
+        required_fuel_mass = fuel_mass(required_fuel_mass)
+
+    return total_mass
+
 
 if __name__ == "__main__":
-    print('Hello world!')
+    totalFuel = 0
+
+    with open(input_path, 'r') as input_file:
+        for line in input_file:
+            totalFuel += total_fuel_for_mass(int(line))
+
+    print(f'Total fuel: {totalFuel}')
