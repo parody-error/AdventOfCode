877dda0227d694e36d8daf32bc39a2861c1b8abd
parody-error <armsitad@gmail.com>
2019-12-13 22:05:40 -0700

    Solving Day 3 part 2

--- day03.py
+++ day03.py
@@ -1,6 +1,4 @@
-
-
-input_path = 'input_test.txt'
+input_path = 'input.txt'
 
 
 def get_direction_modifiers(direction):
@@ -36,7 +34,7 @@ def trace_wire(wire_path):
     return coordinates
 
 
-if __name__ == '__main__':
+def solve_part_one():
     with open(input_path) as input_file:
         wire1 = input_file.readline().strip()
         wire1_coords = trace_wire(wire1)
@@ -45,10 +43,56 @@ if __name__ == '__main__':
         wire2_coords = trace_wire(wire2)
 
         min_distance = 1 << 63
-        intersections = wire1_coords.intersection( wire2_coords )
+        intersections = wire1_coords.intersection(wire2_coords)
         for intersection in intersections:
             current_distance = intersection_distance(intersection)
-            if current_distance < min_distance:
-                min_distance = current_distance
+            min_distance = min(min_distance, current_distance)
 
         print(min_distance)
+
+
+def get_intersection_steps(wire_path, intersections):
+    intersection_steps = {}
+    current_x = 0
+    current_y = 0
+
+    total = 0
+    directions = wire_path.split(',')
+    for direction in directions:
+        (x_modifier, y_modifier) = get_direction_modifiers(direction)
+        distance = int(direction[1:])
+
+        for pos in range(distance):
+            total += 1
+            current_x += x_modifier
+            current_y += y_modifier
+            if (current_x, current_y) in intersections:
+                if (current_x, current_y) not in intersection_steps:
+                    intersection_steps[(current_x, current_y)] = total
+
+    return intersection_steps
+
+
+def solve_part_two():
+    with open(input_path) as input_file:
+        wire1 = input_file.readline().strip()
+        wire1_coords = trace_wire(wire1)
+
+        wire2 = input_file.readline().strip()
+        wire2_coords = trace_wire(wire2)
+
+        intersections = wire1_coords.intersection(wire2_coords)
+        wire1_steps = get_intersection_steps(wire1, intersections)
+        wire2_steps = get_intersection_steps(wire2, intersections)
+
+        min_distance = 1 << 63
+        for intersection in intersections:
+            current_distance = wire1_steps[intersection] + wire2_steps[intersection]
+            min_distance = min(min_distance, current_distance)
+
+        return min_distance
+
+
+if __name__ == '__main__':
+    result = solve_part_two()
+    print(result)
